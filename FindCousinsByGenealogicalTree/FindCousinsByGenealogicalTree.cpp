cdfefde2dfe259054bcf39ea431716742f9f5731
Folonder <olroldugin@yandex.ru>
2023-05-25 12:41:34 +0300

    add read_xml_file func

--- FindCousinsByGenealogicalTree/FindCousinsByGenealogicalTree.cpp
+++ FindCousinsByGenealogicalTree/FindCousinsByGenealogicalTree.cpp
@@ -1,20 +1,35 @@
-﻿// FindCousinsByGenealogicalTree.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "FindCousinsByGenealogicalTree.h"
 
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try
+    {
+        char* input_file = (char*)"data.xml";
+        xml_document<> doc;
+        string xml = read_xml_file(input_file);
+        doc.parse<0>((char*)xml.c_str());
+    }
+    catch (FileNotFoundException ex)
+    {
+        cout << ex.what();
+    }
+    catch (KinshipDegreeException ex)
+    {
+        cout << ex.what();
+    }
+    catch (runtime_error ex)
+    {
+        cout << ex.what();
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+string read_xml_file(char* file_name) {
+    if (!std::ifstream(file_name)) {
+        throw FileNotFoundException(file_name);
+    }
+    file<> xml_file(file_name);
+    return xml_file.data();
+}
--- FindCousinsByGenealogicalTree/FindCousinsByGenealogicalTree.h
+++ FindCousinsByGenealogicalTree/FindCousinsByGenealogicalTree.h
@@ -1,1 +1,11 @@
-#pragma once
+#include <iostream>
+#include "../rapidxml/rapidxml.hpp"
+#include "../rapidxml/rapidxml_utils.hpp"
+#include "../Exceptions/FileNotFoundException.h"
+#include "../Exceptions/KinshipDegreeException.h"
+
+using namespace std;
+using namespace rapidxml;
+
+
+string read_xml_file(char* file_name);
