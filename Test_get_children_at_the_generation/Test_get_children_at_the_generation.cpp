15d1ae8c1427dab1249c333a2f49b9aba9eae957
Folonder <olroldugin@yandex.ru>
2023-06-01 19:22:45 +0300

    add test to get_children_at_the_generation func

---
+++ Test_get_children_at_the_generation/Test_get_children_at_the_generation.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FindCousinsByGenealogicalTree/FindCousinsByGenealogicalTree.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testgetchildrenatthegeneration
+{
+	TEST_CLASS(Testgetchildrenatthegeneration)
+	{
+	public:
+		
+		TEST_METHOD(DefaultTest)
+		{
+			xml_document<> doc;
+			string xml = "<a1><aa2><aaa3/></aa2></a1>";
+			doc.parse<0>((char*)xml.c_str());
+			xml_node<>* root = doc.first_node();
+			vector<xml_node<>*> cousins;
+			get_children_at_generation(root, 2, cousins);
+			Assert::AreEqual("aaa3", cousins[0]->name());
+		}
+
+		TEST_METHOD(GenerationHigherThenTreeDeep)
+		{
+			xml_document<> doc;
+			string xml = "<a1><aa2><aaa3/></aa2></a1>";
+			doc.parse<0>((char*)xml.c_str());
+			xml_node<>* root = doc.first_node();
+			vector<xml_node<>*> cousins;
+			get_children_at_generation(root, 3, cousins);
+			Assert::IsTrue(0 == cousins.size());
+		}
+
+		TEST_METHOD(CousinsOnRight)
+		{
+			xml_document<> doc;
+			string xml = "<a1><aa2></aa2><ab2><aba3/><aab3/></ab2></a1>";
+			doc.parse<0>((char*)xml.c_str());
+			xml_node<>* root = doc.first_node();
+			vector<xml_node<>*> cousins;
+			get_children_at_generation(root, 2, cousins);
+			Assert::AreEqual("aba3", cousins[0]->name());
+			Assert::AreEqual("aab3", cousins[1]->name());
+		}
+	};
+}
