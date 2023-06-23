b449f6d214bd19f24893061663468488b1aa44f9
Folonder <74664782+Folonder@users.noreply.github.com>
2023-06-23 14:10:09 +0300

    add tests to exe file

---
+++ Tests/start.bat
@@ -0,0 +1,7 @@
+FindCousinsByGenealogicalTree.exe test1.xml test1.txt > results1.txt
+
+FindCousinsByGenealogicalTree.exe test3.xml test3.txt > results3.txt
+
+FindCousinsByGenealogicalTree.exe test2.xml test2.txt > results2.txt
+
+
